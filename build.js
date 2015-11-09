2a0830171db5e72f94ec065c43219f5a58f79fe5
Tom MacWright <tom@macwright.org>
2015-11-09 18:37:12 -0500

    New website

--- build.js
+++ build.js
@@ -1,24 +1,31 @@
 var metadata = require('geojson-xyz-data'),
+  path = require('path'),
   Handlebars = require('handlebars'),
   prettyBytes = require('pretty-bytes'),
   fs = require('fs');
 
-for (var k in metadata) {
-  for (var v in metadata[k].files) {
-    for (var i in metadata[k].files[v]) {
-      for (var f = 0; f < metadata[k].files[v][i].length; f++) {
-        metadata[k].files[v][i][f] = {
-          url: metadata[k].meta.data + metadata[k].files[v][i][f],
-          name: metadata[k].files[v][i][f],
-          size: prettyBytes(metadata[k].sizes[metadata[k].files[v][i][f]]),
-          summary: metadata[k].summaries[metadata[k].files[v][i][f]],
-          geojsonio: 'http://geojson.io/#data=data:text/x-url,' +
-            encodeURIComponent(metadata[k].meta.data + metadata[k].files[v][i][f])
-        };
-      }
-    }
-  }
-}
+Handlebars.registerHelper('clean_name', function(text) {
+  return new Handlebars.SafeString(text
+    .replace(/ne_(\d+)m_/, '')
+    .replace(/\.geojson/, '')
+    .replace(/_/g, ' '));
+});
+
+Handlebars.registerHelper('geojsonio', function(text) {
+  return new Handlebars.SafeString('http://geojson.io/#data=data:text/x-url,' + encodeURIComponent(text));
+});
+
+Handlebars.registerHelper('basename', function(text) {
+  return new Handlebars.SafeString(path.basename(text));
+});
+
+Handlebars.registerHelper('resolution', function(text) {
+  return new Handlebars.SafeString(text.match(/ne_(\d+)/)[1]);
+});
+
+Handlebars.registerHelper('prettyBytes', function(text) {
+  return new Handlebars.SafeString(prettyBytes(text));
+});
 
 fs.writeFileSync('index.html',
   Handlebars.compile(fs.readFileSync('./template.hbs', 'utf8'))({
