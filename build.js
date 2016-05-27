be5b6daef08b8a375dc920d5922791f11da393b6
Tom MacWright <tom@macwright.org>
2016-05-27 16:33:35 -0400

    Less nonsense

--- build.js
+++ build.js
@@ -1,39 +1,43 @@
 var metadata = require('geojson-xyz-data'),
+  _ = require('lodash'),
   path = require('path'),
-  Handlebars = require('handlebars'),
   prettyBytes = require('pretty-bytes'),
   fs = require('fs');
+
+var sources = [];
 
 for (var s in metadata) {
   metadata[s].files.forEach(function (file) {
     file.url = file.url.replace('http://geojson.xyz/', 'https://d2ad6b4ur7yvpq.cloudfront.net/');
   });
+  sources.push(metadata[s]);
 }
 
-Handlebars.registerHelper('clean_name', function(text) {
-  return new Handlebars.SafeString(text
+function cleanName(text) {
+  return text
     .replace(/ne_(\d+)m_/, '')
     .replace(/\.geojson/, '')
-    .replace(/_/g, ' '));
-});
+    .replace(/_/g, ' ');
+}
 
-Handlebars.registerHelper('geojsonio', function(text) {
-  return new Handlebars.SafeString('http://geojson.io/#data=data:text/x-url,' + encodeURIComponent(text));
-});
+function geojsonio(text) {
+  return 'http://geojson.io/#data=data:text/x-url,' + encodeURIComponent(text);
+}
 
-Handlebars.registerHelper('basename', function(text) {
-  return new Handlebars.SafeString(path.basename(text));
-});
+function basename(text) {
+  return path.basename(text);
+}
 
-Handlebars.registerHelper('resolution', function(text) {
-  return new Handlebars.SafeString(text.match(/ne_(\d+)/)[1]);
-});
-
-Handlebars.registerHelper('prettyBytes', function(text) {
-  return new Handlebars.SafeString(prettyBytes(text));
-});
+function resolution(text) {
+  return text.match(/ne_(\d+)/)[1];
+}
 
 fs.writeFileSync('index.html',
-  Handlebars.compile(fs.readFileSync('./template.hbs', 'utf8'))({
-    metadata: metadata
+  _.template(fs.readFileSync('./template._', 'utf8'))({
+    sources: sources,
+    resolution: resolution,
+    prettyBytes: prettyBytes,
+    geojsonio: geojsonio,
+    basename: basename,
+    cleanName: cleanName
   }));
